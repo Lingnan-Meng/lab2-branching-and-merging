bed96ac8c9bdb0ca8488fe145eb99033c88edfec
qq2480260359 <lingnanmeng12@gmail.com>
2026-09-21 16:01:11 -0400

    fixed the code

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -4,7 +4,7 @@ public class DataTypes {
         int s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int i = 0; i < numbers.length; i++) {
-            s += x;
+            s += i;
         }
         return s;
     }
